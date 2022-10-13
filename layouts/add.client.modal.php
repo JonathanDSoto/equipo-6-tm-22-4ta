9dd20cf2c6065450f68d432ce77fdc9a7e37dd45
Jonathan Leon <jonathan2610leon@gmail.com>
2022-10-13 17:30:26 -0600

    Vista de clientes

---
+++ layouts/add.client.modal.php
@@ -0,0 +1,49 @@
+<div class="modal fade" id="add-client" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+		    <div class="modal-header">
+		        <h5 class="modal-title" id="exampleModalLabel">Ingresar un Nuevo Cliente</h5>
+		        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+		    </div>
+
+		    <form method="" action="">
+			    <div class="modal-body">
+			      	<div class="input-group mb-3">
+					    <span class="input-group-text" id="basic-addon1">Nombre</span>
+					    <input id="name" name="name" type="text" class="form-control" placeholder="Nombre del cliente" aria-label="Username" aria-describedby="basic-addon1">
+					</div>
+
+					<div class="input-group mb-3">
+					    <span class="input-group-text" id="basic-addon1">Correo Electrónico</span>
+					    <input id="email" name="mail" type="text" class="form-control" placeholder="Correo Electronico" aria-label="Username" aria-describedby="basic-addon1">
+					</div>
+
+					<div class="input-group mb-3">
+					    <span class="input-group-text" id="basic-addon1">No. Teléfono</span>
+					    <input id="description" name="description" type="text" class="form-control" placeholder="No. Telefono" aria-label="Username" aria-describedby="basic-addon1">
+					</div>
+
+					<div class="input-group mb-3">
+					    <span class="input-group-text" id="basic-addon1">Nivel de Suscripción</span>
+					    <select id="level_id" class="form-control" name="level_id">
+                            <option value="">Escoge una Opcion</option>
+                            <option value="">Normal</option>
+                            <option value="">Premiun</option>
+                            <option value="">VIP</option>
+                        </select>
+					</div>				
+
+			      </div>
+			      <div class="modal-footer">
+			        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cerrar</button>
+			        <button type="submit" class="btn btn-primary">Guardar</button>
+			      </div>
+
+			      <input id="oculto_input" type="hidden" name="action" value="create">
+
+			      <input type="hidden" id="id" name="id">
+
+		      </form>
+		    </div>
+		  </div>
+		</div>
---
+++ users/index.php
@@ -0,0 +1,136 @@
+<?php 
+
+	include "../app/ProductsController.php";
+	include "../app/BrandController.php";
+
+	$productController = new ProductsController();
+
+	$brandController = new BrandController();
+
+	// $products = $productController->getProducts();
+	// $brands = $brandController->getBrands();
+
+	#echo json_encode($_SESSION);
+?> 
+<!doctype html>
+<html lang="en" data-layout="vertical" data-topbar="light" data-sidebar="dark" data-sidebar-size="lg" data-sidebar-image="none" data-preloader="disable">
+
+<head>
+
+	<?php include "../layouts/head.template.php"; ?>
+
+    <!-- nouisliderribute css -->
+    <link rel="stylesheet" href="<?= BASE_PATH ?>public/libs/nouislider/nouislider.min.css">
+
+    <!-- gridjs css -->
+    <link rel="stylesheet" href="<?= BASE_PATH ?>public/libs/gridjs/theme/mermaid.min.css">
+</head>
+
+<body>
+    <!-- Begin page -->
+    <div id="layout-wrapper">
+
+    	<?php include "../layouts/nav.template.php"; ?>
+        
+        <!-- ========== App Menu ========== -->
+        <?php include "../layouts/sidebar.template.php";?>
+        <?php include "../layouts/add.client.modal.php";?>
+
+        <div class="main-content">
+
+            <?php include "../layouts/bread.template.php"; ?>
+            <!-- End Page-content -->
+            <!-- Tables Without Borders -->
+            <div class="row ms-2"> 
+                <div class="col-xl-12 col-lg-12">
+                    <div>
+                        <div class="card">
+                            <div class="card-header border-0">
+                                <div class="row g-4">
+                                    <div class="col-sm-auto">
+                                        <div>
+                                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#add-client">
+                                            Añadir Cliente
+                                        </button>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="card-header">
+                                <div class="row align-items-center">
+                                    <div class="col">
+                                        <ul class="nav nav-tabs-custom card-header-tabs border-bottom-0" role="tablist">
+                                            <li class="nav-item">
+                                                <a class="nav-link active fw-semibold" data-bs-toggle="tab" href="#productnav-all" role="tab">
+                                                    Clientes <span class="badge badge-soft-danger align-middle rounded-pill ms-1"># de Clientes</span>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- end card header -->
+                            <table class="table table-borderless table-nowrap">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Id</th>
+                                        <th scope="col">Nombre</th>
+                                        <th scope="col">No. Teléfono</th>
+                                        <th scope="col">Nivel de Subscripción</th>
+                                        <th scope="col">Ordenes</th>
+                                        <th scope="col">Acciones</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">1</th>
+                                        <td>Annette Black</td>
+                                        <td>6121234567</td>
+                                        <td>Normal</td>
+                                        <td>
+                                            <a type="button" class="badge badge-soft-primary">
+                                                <i class="mdi mdi-square-edit-outline"></i> Ver Ódenes
+                                            </a></td>
+                                        <td>
+                                            <div class="hstack gap-3 fs-15">
+                                                <a href="javascript:void(0);" class="link-secondary" data-bs-toggle="modal" data-bs-target="#add-client"><i class="ri-settings-4-line"></i></a>
+                                                <a href="javascript:void(0);" class="link-danger"><i class="ri-delete-bin-5-line"></i></a>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <?php include "../layouts/footer.template.php"; ?>
+        </div>
+    </div>
+
+
+
+    <!--start back-to-top-->
+    <button onclick="topFunction()" class="btn btn-danger btn-icon" id="back-to-top">
+        <i class="ri-arrow-up-line"></i>
+    </button>
+    <!--end back-to-top-->
+
+    <?php include "../layouts/scripts.template.php"; ?>
+
+    <!-- nouisliderribute js -->
+    <script src="<?= BASE_PATH ?>public/libs/nouislider/nouislider.min.js"></script>
+    <script src="<?= BASE_PATH ?>public/libs/wnumb/wNumb.min.js"></script>
+    
+    <!-- gridjs js -->
+    <script src="<?= BASE_PATH ?>public/libs/gridjs/gridjs.umd.js"></script>
+    <!-- ecommerce product list -->
+    <script src="<?= BASE_PATH ?>public/js/pages/ecommerce-product-list.init.js"></script>
+
+
+</body>
+
+
+</html>
