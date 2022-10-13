3f23943876ce5ca55887246511d468a9cba93c6a
Jonathan Leon <jonathan2610leon@gmail.com>
2022-10-13 16:21:41 -0600

    Update add.product.modal.php

--- layouts/add.product.modal.php
+++ layouts/add.product.modal.php
@@ -45,8 +45,8 @@
 
 			      </div>
 			      <div class="modal-footer">
-			        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
-			        <button type="submit" class="btn btn-primary">Save changes</button>
+			        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cerrar</button>
+			        <button type="submit" class="btn btn-primary">Guardar</button>
 			      </div>
 
 			      <input id="oculto_input" type="hidden" name="action" value="create">
