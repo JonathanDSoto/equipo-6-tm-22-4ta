429a84b72d7c1e5692fc336c594e0609d1ae7585
joel <joel.uss1728@gmail.com>
2022-10-14 17:07:07 -0500

    detalle de cliente en especifico

--- app/ClientController.php
+++ app/ClientController.php
@@ -258,6 +258,40 @@ class ClientController{
         // print_r ($newArray;
          return $newArray;
       }
+
+
+      public static function getClient($id){
+                
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://crud.jonathansoto.mx/api/clients/'.$id,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        CURLOPT_HTTPHEADER => array(
+            'Authorization: Bearer '.$_SESSION['token'],
+            'Content-Type: application/x-www-form-urlencoded'
+        ),
+        ));
+      
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        $response = json_decode($response);
+        if(isset($response->code) && $response->code > 0 ){
+            return $response->data;
+        }else{
+            return array();
+        }
+
+                    
+      }
 }
 
 
