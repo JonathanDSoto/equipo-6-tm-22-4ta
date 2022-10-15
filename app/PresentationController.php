e1e770a485d9c5b242ad8745574eb27a3162f894
joel <joel.uss1728@gmail.com>
2022-10-15 02:34:55 -0500

    eliminar presentacion

--- app/PresentationController.php
+++ app/PresentationController.php
@@ -2,7 +2,7 @@
 
 class PresentationController{
 
-
+//	print_r( PresentationController::getAll(1) );
     public static function getAll($productId){
         
         $curl = curl_init();
@@ -31,6 +31,35 @@ class PresentationController{
             return array();
         }
 
+    }
+
+    public static function delete($presentationId){
+        
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://crud.jonathansoto.mx/api/presentations/'.$presentationId,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'DELETE',
+        CURLOPT_HTTPHEADER => array(
+            'Authorization: Bearer '.$_SESSION['token']
+        ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        $response = json_decode($response);
+        if(isset($response->code) && $response->code > 0 ){
+            return $response;
+        }else{
+            return array();
+        }
     }
 }
 
