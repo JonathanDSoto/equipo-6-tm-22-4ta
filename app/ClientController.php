f5df5a84c7d3a8d898dd8f8a0f29da41c6df485e
joel <joel.uss1728@gmail.com>
2022-10-16 20:09:47 -0500

    obtener direcciones de un cliente

--- app/ClientController.php
+++ app/ClientController.php
@@ -361,10 +361,31 @@ class ClientController{
             return $response->data;
         }else{
             return array();
-        }
-
-                    
+        }                    
       }
+    //   print_r(ClientController::getAddresses(1));
+      public static function getAddresses($client_id){
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://crud.jonathansoto.mx/api/addresses/clients/'.$client_id,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        CURLOPT_HTTPHEADER => array(
+            'Authorization: Bearer '.$_SESSION['token'],
+        ),
+        ));
+        $response = curl_exec($curl);
+        curl_close($curl);
+        // $response = json_encode($response);
+        echo $response;
+        // return $response;
+    }
 }
 
 
