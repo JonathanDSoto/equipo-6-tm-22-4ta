09d39dd36b027e8b0416627ce98d2ef00cb2007e
joel <joel.uss1728@gmail.com>
2022-10-16 19:37:55 -0500

    obtener dats de un solo usuario

--- app/UserController.php
+++ app/UserController.php
@@ -267,5 +267,28 @@ public static function delete($id){
         return $response;
     }
 
+    //print_r(UserController::get(1));
+    public static function get($id){
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://crud.jonathansoto.mx/api/users/'.$id,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        CURLOPT_HTTPHEADER => array(
+            'Authorization: Bearer '.$_SESSION['token'],
+        ),
+        ));
+
+        $response = curl_exec($curl);
+        curl_close($curl);
+        return $response;
+    }
+
 }
 ?>
