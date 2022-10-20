ed5c0c2f4a01e3465e14e9f5ebfe259c8fef78b6
Jonathan Leon <jonathan2610leon@gmail.com>
2022-10-19 21:15:02 -0600

    añadir catalogo

---
+++ catalog/categories/index.php
@@ -0,0 +1,118 @@
+<?php 
+    include_once "../../app/config.php";
+?>
+<!doctype html>
+<html lang="en" data-layout="vertical" data-topbar="light" data-sidebar="dark" data-sidebar-size="lg" data-sidebar-image="none" data-preloader="disable">
+
+<head>
+
+	<?php include "../../layouts/head.template.php"; ?>
+
+    <!-- nouisliderribute css -->
+    <link rel="stylesheet" href="<?= BASE_PATH ?>public/libs/nouislider/nouislider.min.css">
+
+    <!-- gridjs css -->
+    <link rel="stylesheet" href="<?= BASE_PATH ?>public/libs/gridjs/theme/mermaid.min.css">
+</head>
+
+<body>
+    <!-- Begin page -->
+    <div id="layout-wrapper">
+
+
+        <!-- ========== App Menu ========== -->
+        <?php include "../../layouts/sidebar.template.php";?>
+        <?php include "../../layouts/add.categorie.modal.php";?>
+        
+
+        <div class="main-content">
+
+            <?php include "../../layouts/bread.template.php"; ?>
+            <!-- End Page-content -->
+            <!-- Tables Without Borders -->
+            <div class="row ms-2"> 
+                <div class="col-xl-12 col-lg-12">
+                    <div>
+                        <div class="card">
+                            <div class="card-header border-0">
+                                <div class="row g-4">
+                                    <div class="col-sm-auto">
+                                        <div>
+                                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#add-categorie">
+                                            Añadir nueva categoria
+                                        </button>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="card-header">
+                                <div class="row align-items-center">
+                                    <div class="col">
+                                        <ul class="nav nav-tabs-custom card-header-tabs border-bottom-0" role="tablist">
+                                            <li class="nav-item">
+                                                <a class="nav-link active fw-semibold" data-bs-toggle="tab" href="#productnav-all" role="tab">
+                                                Categorias <span class="badge badge-soft-danger align-middle rounded-pill ms-1"># de Categorias</span>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- end card header -->
+                            <table class="table table-borderless table-nowrap">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Id</th>
+                                        <th scope="col">Nombre</th>
+                                        <th scope="col">Descripcion</th>
+                                        <th scope="col">Slug</th>
+                                        <th scope="col">Acciones</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+                                            <tr>
+                                            <th scope="row">1</th>
+                                            <td>Dulces y caramelos</td>
+                                            <td>malisiosos viscos </td>
+                                            <td>Dulces-y-caramelos</td>
+                                            <td>
+                                                <div class="hstack gap-3 fs-15">
+                                                    <a href="javascript:void(0);" class="link-secondary" data-bs-toggle="modal" data-bs-target="#add-categorie"><i class="ri-settings-4-line"></i></a>
+                                                    <a href="javascript:void(0);" class="link-danger"><i class="ri-delete-bin-5-line"></i></a>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <?php include "../../layouts/footer.template.php"; ?>
+        </div>
+    </div>
+
+
+
+    <!--start back-to-top-->
+    <button onclick="topFunction()" class="btn btn-danger btn-icon" id="back-to-top">
+        <i class="ri-arrow-up-line"></i>
+    </button>
+    <!--end back-to-top-->
+
+    <?php include "../../layouts/scripts.template.php"; ?>
+
+    <!-- nouisliderribute js -->
+    <script src="<?= BASE_PATH ?>public/libs/nouislider/nouislider.min.js"></script>
+    <script src="<?= BASE_PATH ?>public/libs/wnumb/wNumb.min.js"></script>
+    
+    <!-- gridjs js -->
+    <script src="<?= BASE_PATH ?>public/libs/gridjs/gridjs.umd.js"></script>
+    <!-- ecommerce product list -->
+    <script src="<?= BASE_PATH ?>public/js/pages/ecommerce-product-list.init.js"></script>
+    
+</body>
+</html>
---
+++ layouts/add.categorie.modal.php
@@ -0,0 +1,35 @@
+<div class="modal fade" id="add-categorie" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+		    <div class="modal-header">
+		        <h5 class="modal-title" id="exampleModalLabel">Ingresar una Nueva Categoria</h5>
+		        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+		    </div>
+
+		    <form enctype="multipart/form-data">
+			    <div class="modal-body">
+			      	<div class="input-group mb-3">
+					    <span class="input-group-text" id="basic-addon1">Nombre</span>
+					    <input id="name" name="name" type="text" class="form-control" placeholder="Nombre de la categoria" aria-label="Username" aria-describedby="basic-addon1">
+					</div>
+
+					<div class="input-group mb-3">
+					    <span class="input-group-text" id="basic-addon1">Descripcion</span>
+					    <input id="description" name="description" type="text" class="form-control" placeholder="Descripción de la categoria" aria-label="Username" aria-describedby="basic-addon1">
+					</div>
+
+					<div class="input-group mb-3">
+					    <span class="input-group-text" id="basic-addon1">Slug</span>
+					    <input id="slug" name="slug" type="text" class="form-control" placeholder="Slug de la categoria" aria-label="Username" aria-describedby="basic-addon1">
+					</div>
+					
+			    </div>
+			    <div class="modal-footer">
+			        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cerrar</button>
+			        <button type="submit" class="btn btn-primary">Guardar</button>
+			    </div>
+
+		    </form>
+		</div>
+	</div>
+</div>
