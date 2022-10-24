c7b8a2f69c565c6c3b6d3f6a9ed8d03d487fb62e
joel <joel.uss1728@gmail.com>
2022-10-24 14:36:33 -0500

    crud de ordeenes

--- app/OrderController.php
+++ app/OrderController.php
@@ -20,6 +20,61 @@ if (isset($_POST['action'])) {
                  header('Location: '.$_SERVER['HTTP_REFERER']);  
                     break;
                 }
+
+                case 'create':{
+                    
+                    /* 
+                    public static function create($folio, $total, $is_paid, $client_id, $address_id, $order_status_id, 
+                    $payment_type_id, $coupon_id, $presentations){ */
+                    $folio = strip_tags(trim($_POST['folio']));
+                    $total = strip_tags(trim($_POST['total']));
+                    $is_paid = strip_tags(trim($_POST['is_paid']));
+                    $client_id = strip_tags(trim($_POST['client_id']));
+                    $address_id = strip_tags(trim($_POST['address_id']));
+                    $order_status_id = strip_tags(trim($_POST['order_status_id']));
+                    $payment_type_id = strip_tags(trim($_POST['payment_type_id']));
+                    $coupon_id = strip_tags(trim($_POST['coupon_id']));
+                    $presentations = $_POST['presentations'];
+                    
+                    $validationResult = Validator::createOrder($folio, $total, $is_paid, $client_id, $address_id, $order_status_id, $payment_type_id, $coupon_id);
+/*                     $presentations = [
+                        ['id' => 61, 'quantity' => 1],
+                        ['id' => 62, 'quantity' => 1]
+                    ]; */
+                    $validOrderStatusId = ($order_status_id > 0 && $order_status_id < 7);
+                    $validPresentations = Validator::kwd_integer_array($presentations);
+                    if($validPresentations['status'] == '1' && $validOrderStatusId && $validationResult['status']== '1'){
+                            $_SESSION['_MESSAGE'] = OrderController::create($folio, $total, $is_paid, $client_id, $address_id, $order_status_id, $payment_type_id, $coupon_id, $presentations);
+                    }else{
+                        $_SESSION['_MESSAGE'] = $validationResult['data'];
+                    }
+
+
+                    header('Location: '.$_SERVER['HTTP_REFERER']);  
+                    break;
+                }
+
+                case 'update':{
+
+                    $order_id = $_POST['order_id'];
+                    $order_status_id = $_POST['order_status_id'];
+
+                    if($order_status_id>0 && $order_status_id<7){ // el estado de la orden solo puede tener valores en este rango
+                        $_SESSION['_MESSAGE'] = OrderController::update($order_id, $order_status_id);
+                    }
+                    header('Location: '.$_SERVER['HTTP_REFERER']);  
+                    break;
+                }
+
+                case 'delete':{
+                    $order_id = $_POST['order_id'];
+                    $validId = Validator::integer($order_id);
+                    if($validId){
+                        $_SESSION['_MESSAGE'] = OrderController::delete($order_id);
+                    }else{
+                        $_SESSION['_MESSAGE'] = 'Hay algo mal con ese ID';
+                    }
+                }
             }
         }
 }
@@ -118,7 +173,124 @@ class OrderController{
             }
         }
         return $filteredOrders;
+    }
 
+    public static function create($folio, $total, $is_paid, $client_id, $address_id, $order_status_id, 
+    $payment_type_id, $coupon_id, $presentations){
+        
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://crud.jonathansoto.mx/api/orders',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => array('folio' => $folio,
+        'total' => $total,
+        'is_paid' => $is_paid,
+        'client_id' => $client_id,
+        'address_id' => $address_id,
+        'order_status_id' => $order_status_id,
+        'payment_type_id' => $payment_type_id,
+        'coupon_id' => $coupon_id,
+        'presentations[0][id]' => $presentations[0]['id'],
+        'presentations[0][quantity]' => $presentations[0]['quantity'], 
+        'presentations[1][id]' => $presentations[1]['id'],
+        'presentations[1][quantity]' => $presentations[1]['quantity']),
+       
+       
+        CURLOPT_HTTPHEADER => array(
+            'Authorization: Bearer '.$_SESSION['token']
+        ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+ 
+ //       echo $response;
+        $response = json_encode($response);
+        return $response;
+    }
+
+
+    public static function getDetails($order_id){
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+    CURLOPT_URL => 'https://crud.jonathansoto.mx/api/orders/details/'.$order_id,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'GET',
+    CURLOPT_HTTPHEADER => array(
+        'Authorization: Bearer '.$_SESSION['token']
+    ),
+    ));
+
+    $response = curl_exec($curl);
+
+    curl_close($curl);
+    return $response;
+
+
+    }
+
+
+    public static function update($order_id, $order_status_id){
+                
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://crud.jonathansoto.mx/api/orders',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'PUT',
+        CURLOPT_POSTFIELDS => 'id='.$order_id.'&order_status_id='.$order_status_id.'',
+        CURLOPT_HTTPHEADER => array(
+            'Authorization: Bearer '.$_SESSION['token']
+        ),
+        ));
+
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        return  $response;
+
+    }
+
+    public static function delete($order_id){
+        
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://crud.jonathansoto.mx/api/orders/'.$order_id,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'DELETE',
+        CURLOPT_HTTPHEADER => array(
+            'Authorization: Bearer '.$_SESSION['token']
+        ),
+        ));
+        $response = curl_exec($curl);
+        curl_close($curl);
+        return $response;
 
     }
 }
