a7389907530bf76d8ff0e7d645186cb76f707340
Diego Ochoa <diegochoa0510@gmail.com>
2022-10-24 18:14:44 -0600

    Se añaden categorias

--- layouts/add.categorie.modal.php
+++ layouts/add.categorie.modal.php
@@ -6,21 +6,21 @@
 		        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 		    </div>
 
-		    <form enctype="multipart/form-data">
+		    <form method="post" action="<?php echo BASE_PATH?>ctg">
 			    <div class="modal-body">
 			      	<div class="input-group mb-3">
 					    <span class="input-group-text" id="basic-addon1">Nombre</span>
-					    <input id="name" name="name" type="text" class="form-control" placeholder="Nombre de la categoria" aria-label="Username" aria-describedby="basic-addon1">
+					    <input id="idName" name="name" type="text" class="form-control" placeholder="Nombre de la categoria" aria-label="Username" aria-describedby="basic-addon1">
 					</div>
 
 					<div class="input-group mb-3">
 					    <span class="input-group-text" id="basic-addon1">Descripcion</span>
-					    <input id="description" name="description" type="text" class="form-control" placeholder="Descripción de la categoria" aria-label="Username" aria-describedby="basic-addon1">
+					    <input id="idDescription" name="description" type="text" class="form-control" placeholder="Descripción de la categoria" aria-label="Username" aria-describedby="basic-addon1">
 					</div>
 
 					<div class="input-group mb-3">
 					    <span class="input-group-text" id="basic-addon1">Slug</span>
-					    <input id="slug" name="slug" type="text" class="form-control" placeholder="Slug de la categoria" aria-label="Username" aria-describedby="basic-addon1">
+					    <input id="idSlug" name="slug" type="text" class="form-control" placeholder="Slug de la categoria" aria-label="Username" aria-describedby="basic-addon1">
 					</div>
 					
 			    </div>
@@ -28,6 +28,10 @@
 			        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cerrar</button>
 			        <button type="submit" class="btn btn-primary">Guardar</button>
 			    </div>
+
+				<input type="hidden" name="action" value="create" id="oculto_input">
+				<input type="hidden" value="1" name="category_id" id="category_id">
+				<input type="hidden" name="global_token" value="<?php echo $_SESSION['global_token'] ?>">
 
 		    </form>
 		</div>
