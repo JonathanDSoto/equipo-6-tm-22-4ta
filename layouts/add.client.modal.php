853ebb196bfc75446a6e24bc7945ed404ce187fa
Jesus Carlos <contacto0carlos@gmail.com>
2022-10-26 16:42:23 -0600

    Se añaden clientes

--- layouts/add.client.modal.php
+++ layouts/add.client.modal.php
@@ -6,7 +6,7 @@
 		        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 		    </div>
 
-		    <form method="" action="">
+		    <form method="post" action="<?php echo BASE_PATH?>cln"> 
 			    <div class="modal-body">
 			      	<div class="input-group mb-3">
 					    <span class="input-group-text" id="basic-addon1">Nombre</span>
@@ -17,7 +17,7 @@
 					    <span class="input-group-text" id="basic-addon1">Correo Electrónico</span>
 					    <input id="email" name="email" type="text" class="form-control" placeholder="Correo Electronico" aria-label="Username" aria-describedby="basic-addon1">
 					</div>
-					
+
 					<div class="input-group mb-3">
 					    <span class="input-group-text" id="basic-addon1">Contraseña</span>
 					    <input id="password" name="password" type="text" class="form-control" placeholder="Contraseña" aria-label="Username" aria-describedby="basic-addon1">
@@ -36,13 +36,17 @@
                             <option value="">Premiun</option>
                             <option value="">VIP</option>
                         </select>
-					</div>				
+					</div>
 
 			    </div>
 			    <div class="modal-footer">
 			        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cerrar</button>
 			        <button type="submit" class="btn btn-primary">Guardar</button>
 			    </div>
+
+					<input type="hidden" name="action" value="create" id="oculto_input">
+					<input type="hidden" name="global_token" value="<?php echo $_SESSION['global_token'] ?>">
+
 		    </form>
 		</div>
 	</div>
