a867ec112a32774e32ca763d7c3d419bb9402a0a
Jonathan Leon <jonathan2610leon@gmail.com>
2022-10-30 19:54:51 -0600

    Create edit.address.modal.php

---
+++ layouts/edit.address.modal.php
@@ -0,0 +1,53 @@
+<div class="modal fade" id="edit-address" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+		    <div class="modal-header">
+		        <h5 class="modal-title" id="exampleModalLabel">Editar una direccion</h5>
+		        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+		    </div>
+
+		    <form method="" action="">
+			    <div class="modal-body">
+                <div class="input-group mb-3">
+					    <span class="input-group-text" id="basic-addon1">Nombre(s)</span>
+					    <input id="" name="" type="text" class="form-control" placeholder="Nombre del residente" aria-label="Username" aria-describedby="basic-addon1">
+					</div>
+                    <div class="input-group mb-3">
+					    <span class="input-group-text" id="basic-addon1">Apellidos</span>
+					    <input id="" name="" type="text" class="form-control" placeholder="Apellidos del residente" aria-label="Username" aria-describedby="basic-addon1">
+					</div>
+
+                    <div class="input-group mb-3">
+					    <span class="input-group-text" id="basic-addon1">Calle y No.</span>
+					    <input id="" name="" type="text" class="form-control" placeholder="Calle y número" aria-label="Username" aria-describedby="basic-addon1">
+					</div>
+
+					<div class="input-group mb-3">
+					    <span class="input-group-text" id="basic-addon1">Código postal</span>
+					    <input id="" name="" type="text" class="form-control" placeholder="Código postal" aria-label="Username" aria-describedby="basic-addon1">
+					</div>
+					
+					<div class="input-group mb-3">
+					    <span class="input-group-text" id="basic-addon1">Ciudad</span>
+					    <input id="" name="" type="text" class="form-control" placeholder="Ciudad" aria-label="Username" aria-describedby="basic-addon1">
+					</div>
+
+					<div class="input-group mb-3">
+					    <span class="input-group-text" id="basic-addon1">Estado</span>
+					    <input id="" name="" type="text" class="form-control" placeholder="Estado" aria-label="Username" aria-describedby="basic-addon1">
+					</div>
+
+                    <div class="input-group mb-3">
+					    <span class="input-group-text" id="basic-addon1">No. Teléfono</span>
+					    <input id="" name="" type="text" class="form-control" placeholder="Número de teléfono" aria-label="Username" aria-describedby="basic-addon1">
+					</div>
+
+			    </div>
+			    <div class="modal-footer">
+			        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cerrar</button>
+			        <button type="submit" class="btn btn-primary">Guardar</button>
+			    </div>
+		    </form>
+		</div>
+	</div>
+</div>
