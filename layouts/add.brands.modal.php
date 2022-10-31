25f3fef91dcbe4cc9de94c56bb49ebd9694bf9cf
Jesus Carlos <contacto0carlos@gmail.com>
2022-10-31 16:14:22 -0700

    pattern added brand modal

--- layouts/add.brands.modal.php
+++ layouts/add.brands.modal.php
@@ -6,23 +6,23 @@
 		        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 		    </div>
 
-		    <form method="post" action="<?php echo BASE_PATH?>brds"> 
+		    <form method="post" action="<?php echo BASE_PATH?>brds">
 			    <div class="modal-body">
 			      	<div class="input-group mb-3">
 					    <span class="input-group-text" id="basic-addon1">Nombre</span>
-					    <input id="name" name="name" type="text" class="form-control" placeholder="Nombre de la marca" aria-label="Username" aria-describedby="basic-addon1">
+					    <input id="name" name="name" pattern="^[a-zA-Z ]+$" type="text" class="form-control" placeholder="Nombre de la marca" aria-label="Username" aria-describedby="basic-addon1" required>
 					</div>
 
 					<div class="input-group mb-3">
 					    <span class="input-group-text" id="basic-addon1">Descripcion</span>
-					    <input id="description" name="description" type="text" class="form-control" placeholder="Descripción de la marca" aria-label="Username" aria-describedby="basic-addon1">
+					    <input id="description" name="description" type="text" class="form-control" placeholder="Descripción de la marca" aria-label="Username" aria-describedby="basic-addon1" required>
 					</div>
 
 					<div class="input-group mb-3">
 					    <span class="input-group-text" id="basic-addon1">Slug</span>
-					    <input id="slug" name="slug" type="text" class="form-control" placeholder="Slug de la marca" aria-label="Username" aria-describedby="basic-addon1">
+					    <input id="slug" name="slug" pattern="^[a-z0-9]+(?:-[a-z0-9]+)*$" type="text" class="form-control" placeholder="Slug de la marca" aria-label="Username" aria-describedby="basic-addon1" required>
 					</div>
-					
+
 			    </div>
 			    <div class="modal-footer">
 			        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cerrar</button>
